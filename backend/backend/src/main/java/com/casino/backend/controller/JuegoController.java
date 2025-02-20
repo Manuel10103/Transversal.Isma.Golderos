b8597b746d51ac74073ddeac8d3ec0a12914ca5a
Manuel10103 <mgolderos66@gmail.com>
2025-02-20 10:02:32 +0100

    Arreglo juego controller

--- backend/backend/src/main/java/com/casino/backend/controller/JuegoController.java
+++ backend/backend/src/main/java/com/casino/backend/controller/JuegoController.java
@@ -2,6 +2,8 @@ package com.casino.backend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class JuegoController {
@@ -14,5 +16,11 @@ public class JuegoController {
     @GetMapping("/juegos/carrera")
     public String mostrarCarreraCaballos() {
         return "juegos/dino"; 
+    }   
+
+    @GetMapping("/ruleta")
+    public String mostrarRuleta() {
+        return "juegos/ruleta";
     }
+
 }
